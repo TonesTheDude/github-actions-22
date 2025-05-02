2d1701f688125f041f8eed2b0e1d90061e2c0157
TonesTheDude <alangdon1@gmail.com>
2025-05-01 21:52:20 -0400

    fixed calc

--- src/tests/Calculator.test.tsx
+++ src/tests/Calculator.test.tsx
@@ -6,7 +6,7 @@ describe('Calculator', () => {
   it('should contain the expected text', () => {
     render(<Calculator />);
 
-    const itemElement = document.querySelector('.calculator');
+    const itemElement = document.querySelector('h2');
 
     if (itemElement) {
       expect(itemElement.textContent).toBe('Simple Calculator');  
